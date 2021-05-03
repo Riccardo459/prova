4f5c6412521ad9d7df1fa2d5ce2cfc7b2681677f
Riccardo459 <Riccardo.Bellan99@gmail.com>
2021-05-03 22:08:57 +0200

    aggiunta classe cpp

---
+++ classeprova.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Riccardo on 03/05/2021.
+//
+
+#include "classeprova.h"
---
+++ classeprova.h
@@ -0,0 +1,16 @@
+//
+// Created by Riccardo on 03/05/2021.
+//
+
+#ifndef PROVA_CLASSEPROVA_H
+#define PROVA_CLASSEPROVA_H
+
+
+
+class classeprova {
+
+};
+
+
+
+#endif //PROVA_CLASSEPROVA_H
